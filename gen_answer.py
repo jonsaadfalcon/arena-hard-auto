c59eba41b2d8f97e26e8c226b2caba0364efe02d
Jon Saad-Falcon <41205309+jonsaadfalcon@users.noreply.github.com>
2024-06-17 10:45:01 -0700

    update

--- gen_answer.py
+++ gen_answer.py
@@ -46,8 +46,6 @@ def get_answer(
     elif model in OPENAI_MODEL_LIST:
         conv.append({"role": "system", "content": "You are a helpful assistant."})
 
-    breakpoint()
-
     encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
     choices = []
     total_model_to_outputs_dict = []
