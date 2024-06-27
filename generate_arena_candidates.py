8c4cf911fa0858b340b0e062cce4f1c1830e81d4
Jon Saad-Falcon <jonsaadfalcon@gmail.com>
2024-06-27 05:05:19 +0000

    update

--- generate_arena_candidates.py
+++ generate_arena_candidates.py
@@ -48,6 +48,7 @@ from datasets import Dataset, concatenate_datasets
 #models = ["Qwen/Qwen1.5-72B-Chat", "Qwen/Qwen1.5-110B-Chat", "microsoft/WizardLM-2-8x22B",
 #          "mistralai/Mixtral-8x22B-Instruct-v0.1", "meta-llama/Llama-3-70b-chat-hf", "databricks/dbrx-instruct"]
 
+
 models = ["Qwen/Qwen1.5-7B-Chat","meta-llama/Meta-Llama-3-8B-Instruct", "Nexusflow/Starling-LM-7B-beta", 
           "berkeley-nest/Starling-LM-7B-alpha", "teknium/OpenHermes-2.5-Mistral-7B", "mistralai/Mistral-7B-Instruct-v0.2",
           "cognitivecomputations/dolphin-2.2.1-mistral-7b", "microsoft/Phi-3-mini-4k-instruct", #"upstage/SOLAR-10.7B-Instruct-v1.0",
